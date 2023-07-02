71a0f94a18da437d68ff8e9090f8e55c49372b87
Florian Hitz <florian@raffiniert.biz>
2023-07-02 10:06:53 +0200

    progress on HeroImageThree component

--- components/HeroImageThree.vue
+++ components/HeroImageThree.vue
@@ -2,6 +2,7 @@
 import { onMounted, onUnmounted, ref } from "vue";
 import { debounce } from "@/functions/main";
 import * as THREE from "three";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const props = defineProps({
   image: String,
@@ -10,13 +11,35 @@ const props = defineProps({
 
 const threeContainer = ref(null);
 
-let camera, renderer, scene;
+let camera, renderer, scene, controls, sphereMesh, segmentSphereMesh;
 
 function handleResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
   renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function handleMouseClick(event) {
+  event.preventDefault();
+
+  // Calculate normalized device coordinates
+  var mouse = new THREE.Vector2();
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  // Perform raycasting from the camera to the clicked position
+  var raycaster = new THREE.Raycaster();
+  raycaster.setFromCamera(mouse, camera);
+
+  // Check for intersection between the ray and the segmentSphere mesh
+  var intersects = raycaster.intersectObject(segmentSphereMesh);
+
+  // If there is an intersection, handle the click event
+  if (intersects.length > 0) {
+    console.log('Segment clicked!');
+    // Perform your desired action here when the segment is clicked
+  }
 }
 
 const debouncedHandleResize = debounce(handleResize, 400);
@@ -39,38 +62,43 @@ onMounted(() => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   threeContainer.value.appendChild(renderer.domElement);
 
-  // Create a cube
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+  // Create controls
+  controls = new OrbitControls(camera, renderer.domElement);
 
   // Create a sphere for the 360 View
-  var sphere = new THREE.SphereGeometry(500, 60, 40);
-  sphere.scale(-1, 1, 1);
+  var sphereGeometry = new THREE.SphereGeometry(500, 60, 40);
+  sphereGeometry.scale(-1, 1, 1);
   var materialSphere = new THREE.MeshBasicMaterial({
     map: new THREE.TextureLoader().load(props.image),
   });
-  var mesh = new THREE.Mesh(sphere, materialSphere);
-  scene.add(mesh);
+  sphereMesh = new THREE.Mesh(sphereGeometry, materialSphere);
+  scene.add(sphereMesh);
+
+  // Create a sphere segment geometry
+  var segmntSphereGeometry = new THREE.SphereGeometry(500, 60, 40, 0.85, 0.7, 1.4, 0.5);
+  var segmntSphereMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.DoubleSide, opacity: 0.5, transparent: true, border: 0x000000 });
+  segmentSphereMesh = new THREE.Mesh(segmntSphereGeometry, segmntSphereMaterial);
+  segmentSphereMesh.material.depthTest = false;
+  segmentSphereMesh.renderOrder = 1;
+  segmentSphereMesh.position.set(0, 20, 0);
+  sphereMesh.add(segmentSphereMesh);
 
   // Render the scene
   function animate() {
     requestAnimationFrame(animate);
 
-    // cube rotation
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
     renderer.render(scene, camera);
   }
 
   animate();
+
   window.addEventListener("resize", debouncedHandleResize);
+  window.addEventListener('click', handleMouseClick);
 });
 
 onUnmounted(() => {
   window.removeEventListener("resize", debouncedHandleResize);
+  window.removeEventListener('click', handleMouseClick);
 });
 </script>
 
