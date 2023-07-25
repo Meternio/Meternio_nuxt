aa195094f40d9179410f863381ee1f4c86178841
Florian Hitz <web.florianhitz@gmail.com>
2023-07-25 17:04:55 +0200

    finished HeaderBugerMenu

--- components/HeaderBurgerMenu.vue
+++ components/HeaderBurgerMenu.vue
@@ -15,7 +15,7 @@ const showOverlay = ref(false);
       ></span>
     </button>
     <nav
-      class="menu-overlay fixed top-0 left-0 w-full h-full z-50 bg-gray flex items-center justify-center flex-col"
+      class="menu-overlay fixed top-0 left-0 w-full h-full z-50 bg-gray flex items-center justify-center flex-col gap-8 text-2xl"
       :class="{
         hidden: !showOverlay,
       }"
@@ -36,35 +36,35 @@ const showOverlay = ref(false);
           <nuxt-img
             src="img/logo_white.webp"
             preload
-            height="60"
-            class="h-10"
+            height="64"
+            class="h-16"
             :alt="$t('header.logo')"
           />
         </NuxtLink>
       </div>
-      <div class="menu-overlay-content text-white flex flex-col">
+      <div class="menu-overlay-content text-white flex flex-col text-center">
         <NuxtLink
           :to="localePath('/experience')"
           role="menuitem"
-          class="mr-2"
           >{{ $t("header.experience") }}</NuxtLink
         >
-        <NuxtLink :to="localePath('/portfolio')" role="menuitem" class="mr-2">{{
+        <NuxtLink :to="localePath('/portfolio')" role="menuitem">{{
           $t("header.portfolio")
         }}</NuxtLink>
-        <NuxtLink :to="localePath('/contact')" role="menuitem" class="mr-2">{{
+        <NuxtLink :to="localePath('/contact')" role="menuitem">{{
           $t("header.contact")
         }}</NuxtLink>
-        <NuxtLink :to="localePath('/blog')" role="menuitem" class="mr-2">{{
+        <NuxtLink :to="localePath('/blog')" role="menuitem">{{
           $t("header.blog")
         }}</NuxtLink>
       </div>
-      <div class="menu-overlay-footer text-white">
-        <NuxtLink :to="switchLocalePath('en')">{{
-          $t("header.language.en")
-        }}</NuxtLink>
+      <div class="menu-overlay-footer text-white flex flex-row gap-2">
         <NuxtLink :to="switchLocalePath('de')">{{
           $t("header.language.de")
+        }}</NuxtLink>
+        <div class="w-px h-7 bg-white"></div>
+        <NuxtLink :to="switchLocalePath('en')">{{
+          $t("header.language.en")
         }}</NuxtLink>
       </div>
     </nav>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="layout relative">
-    <Menu/>
+    <HeaderBurgerMenu/>
     <main>
       <slot />
     </main>
